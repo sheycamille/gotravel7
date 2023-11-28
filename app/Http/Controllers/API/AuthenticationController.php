17e774800e67fc45af53f54a71d2177c9fd9a2c2
akamsr <romanricakam@gmail.com>
2023-11-28 11:31:37 +0100

    adjusted register and login with phone_number

--- app/Http/Controllers/API/AuthenticationController.php
+++ app/Http/Controllers/API/AuthenticationController.php
@@ -55,12 +55,14 @@ class AuthenticationController extends Controller
     public function register(Request $request)
     {
 
+        info($request->all());
+
         $validator = Validator::make($request->all(), [
             'firstname' => 'required|string|max:255',
             'lastname' => 'required|string|max:255',
             'username' => 'required|string|max:255',
             'verifiedWith' => 'required|string',
-            'phone' => 'sometimes|required_without:email|string|max:255|unique:users',
+            'phone_number' => 'sometimes|required_without:email|string|max:255|unique:users',
             'email' => 'sometimes|required_without:phone|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
         ]);
@@ -75,14 +77,14 @@ class AuthenticationController extends Controller
             'first_name'=> $request->firstname,
             'last_name'=> $request->lastname,
             'username'=> $request->username,
-            'phone_number' => $request->phone,
+            'phone_number' => $request->phone_number,
             'email'=> $request->email,
             'password'=> Hash::make($request->password),
         ]);
 
         $code = random_int(100000, 999999);
         if(isset($request->email) && $request->email != '') {
-            User::where('phone_number', $user->phone)->update(['otp' => $code]);
+            User::where('email', $user->email)->update(['otp' => $code]);
             Mail::to($user->email)->send(new VerifyEmail($code, $name));
             return response([
                 "message" => "Email verification sent",
@@ -96,7 +98,7 @@ class AuthenticationController extends Controller
 
         if($request->verifiedWith == 'email'){
             if(isset($request->email) && $request->email != '') {
-                User::where('phone_number', $user->phone)->update(['otp' => $code]);
+                User::where('email', $user->email)->update(['otp' => $code]);
                 Mail::to($user->email)->send(new VerifyEmail($code, $name));
                 return response([
                     "message" => "Email verification sent",
@@ -139,7 +141,7 @@ class AuthenticationController extends Controller
             return response()->json(['message' => $validator->errors()], 401);
         }
 
-        $user = User::where(['email' => $request->email])->first();
+        $user = User::where('email', $request->email)->first();
 
         if($user->otp !== $request->otp){
             return response()->json(['message' => 'Invalid Code'], 401);
--- routes/api.php
+++ routes/api.php
@@ -3,20 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::group(["prefix"=> "user"], function () {
+Route::group(["prefix"=> "auth"], function () {
 
-    Route::group(["prefix"=> "auth"], function () {
-
-        Route::post('register', 'API\AuthenticationController@register');
-        Route::post('login', 'API\AuthenticationController@login');
-        Route::post('verify', 'API\AuthenticationController@emailVerify');
-        Route::post('otp/resend', 'API\AuthenticationController@resendOtp');
-        Route::post('find-account', 'API\PasswordResetController@findAccount');
-        Route::post('reset-password', 'API\PasswordResetController@changePassword');
-        
-    });
-
+    Route::post('register', 'API\AuthenticationController@register');
+    Route::post('login', 'API\AuthenticationController@login');
+    Route::post('verify', 'API\AuthenticationController@emailVerify');
+    Route::post('otp/resend', 'API\AuthenticationController@resendOtp');
+    Route::post('find-account', 'API\PasswordResetController@findAccount');
+    Route::post('reset-password', 'API\PasswordResetController@changePassword');
+    
 });
+
 
 //Route::get('ride-details/{id}', 'API\RideController@details');
 //Route::get('ride-details/{id}', 'API\RideController@details');
